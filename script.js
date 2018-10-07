85acc2649a5326327fb858b67e0a8572cf45a450
AnEntireSkeleton <AnEntireSkeleton@users.noreply.github.com>
2018-10-07 15:29:02 -0700

    Added comments to script.js, adjusted some naming

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function validInput(){
+  //Clears screen of prior outputs
   $( "#table_here" ).empty();
   var m, b, xMin, xMax, content;
-
+  //Recieves each variable from the fields
   m = document.getElementById("m").value;
   b = document.getElementById("b").value;
   xMin = document.getElementById("xMin").value;
   xMax = document.getElementById("xMax").value;
 
+  //Checks validity of the input
   if(isNaN(m) || isNaN(b) || isNaN(xMin) || isNaN(xMax)){
     content = "<p>Please input numbers</p>";
   }else if((Math.abs(m) > 1000) || (Math.abs(b) > 1000) || (Math.abs(xMin) > 1000) || (Math.abs(xMax) > 1000)){
@@ -15,14 +17,16 @@ function validInput(){
     content = "<p>x minimum must be less than x maximum</p>";
   }else if ((xMax - xMin) > 9) {
     content = "<p>Range of x must be no greater than 10</p>";
-  }else{
-    var result
+  }
+  //Fills content variable with a table class, and calculates the contents of said table
+  else{
     content = "<table class = \"table table-hover\"><thread><tr><th>x</th><th>y</th></tr></thread><tbody>";
     for(i = xMin; i <= xMax; i++){
-      result = (i * m + parseInt(b))
-      content += '<tr><th>' + i + '</th><th>' + result + '</th><tr>';
+      content += '<tr><th>' + i + '</th><th>' + (i * m + parseInt(b)) + '</th></tr>';
     }
     content += "</tbody></table>";
   }
+
+  //appends error or table to index.html
   $('#table_here').append(content);
 }
