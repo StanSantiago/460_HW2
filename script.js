dcaeced8c7c81fc7c66ee8107b37f2e54505a125
AnEntireSkeleton <AnEntireSkeleton@users.noreply.github.com>
2018-10-07 19:55:00 -0700

    In script.js forced xMax and xMin to be seen as numbers, fixing an issue with '>' checking as strings

--- script.js
+++ script.js
@@ -13,16 +13,16 @@ function printTable(){
     content = "<p>Please input numbers</p>";
   }else if((Math.abs(m) > 1000) || (Math.abs(b) > 1000) || (Math.abs(xMin) > 1000) || (Math.abs(xMax) > 1000)){
     content = "<p>Please use values between -1000 and 1000</p>";
-  }else if (xMin > xMax) {
+  }else if (Number(xMin) > Number(xMax)) {
     content = "<p>x minimum must be less than x maximum</p>";
-  }else if ((xMax - xMin) > 9) {
+  }else if ((Number(xMax) - Number(xMin)) > 9) {
     content = "<p>Range of x must be no greater than 10</p>";
   }
   //Fills content variable with a table class, and calculates the contents of said table
   else{
     content = "<table class = \"table table-hover\"><thread><tr><th>x</th><th>y</th></tr></thread><tbody>";
-    for(i = xMin; i <= xMax; i++){
-      content += '<tr><th>' + i + '</th><th>' + (i * m + parseInt(b)) + '</th></tr>';
+    for(i = Number(xMin); i <= Number(xMax); i++){
+      content += '<tr><th>' + i + '</th><th>' + (i * Number(m) + parseInt(b)) + '</th></tr>';
     }
     content += "</tbody></table>";
   }
